df9241f5023f0d70ed4d59016f98f1c99c321e78
Immanuel Bagi Suranta <103119692+immbage@users.noreply.github.com>
2023-06-26 09:10:06 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <cstring>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char s[1000];
+    int i,n,c=0;
+
+    printf("Enter the string : ");
+    gets(s);
+    n=strlen(s);
+    printf("length : %d \n", n);
+
+    for(i=0;i<n/2;i++)
+    {
+        if(s[i]==s[n-i-1])
+            c++;
+    }
+    if(c==i)
+        printf("string is palindrome");
+    else
+        printf("string is not palindrome");
+
+    return 0;
+}
